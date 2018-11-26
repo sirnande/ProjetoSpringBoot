ee267b1e60b8e935276ff7f82a10d98112b7c9b0
Sirnande dos Santos Lima <DB1\sirnande.lima@mga10274.db1.com.br>
2018-11-26 09:12:09 -0200

    Comit inicial do s arquivos do projeto

---
+++ src/main/java/br/com/db1/ProjetoSpringBoot/ProjetoSpringBoot/ProjetoSpringBootApplication.java
@@ -0,0 +1,12 @@
+package br.com.db1.ProjetoSpringBoot.ProjetoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBootApplication.class, args);
+	}
+}
